394e8b489b1cbfbe19bc77c01340390b9c1d6cd8
Charly Leal <carlitos007leal@gmail.com>
2023-03-29 10:06:26 -0300

    comienzo de reparacion

--- src/modules/login/layouts/LoginLayout.vue
+++ src/modules/login/layouts/LoginLayout.vue
@@ -1,129 +1,21 @@
 <template >
-  <section class="principal">
-     <div class="menu">
-             <h1>Sistema de Gestión Odontológica</h1>
-             <h3>BIENVENIDO</h3> 
-             <div class="botones">
-               <RouterLink to="/login-auth"><button class="btn-acento">Login</button></RouterLink>
-                       <button class="btn-primary">Registro</button>
+  <div class="login-layout">
+    <router-view></router-view>
+    <h1>Sistema de Gestion Odontologica</h1>
+    <h2>Bienvenido</h2>
+    <RouterLink to="/login-auth"
+      ><button class="login">Login</button></RouterLink
+    >
+    <button>Registro</button>
 
-                      <div class="rectangulo"></div>
-             </div>
-     </div>   
- </section>
+    <div class="rectangulo"></div>
+  </div>
 </template>
 
-<style scoped>.principal {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-image: url("../../../assets/fondo.png");
-  background-size: cover;
-  background-position: center;
-
-}
-.principal .menu {
-  width: 43%;
-  padding: 2% 0%;
-  box-sizing: border-box;
-  text-align: center;
-  position: relative;
-  margin-left: 199px;
-  background: rgba(0, 0, 0, 0.51);
-  line-height: 0;
-}
-
-.menu h1{
-
-  font-style: italic;
-  font-weight: 900;
-  font-size: 60px;
-  line-height: 70px;
-  letter-spacing: 0.16px;
-  color: #ffff;
-}
-.menu h3{
-  font-style: normal;
-  font-weight: 500;
-  line-height: 53px;
-  font-size: 45px;
-  margin-top: 10%;
-  margin-bottom: 10%;
-  padding: 10% 0% 10% 0%;
-  box-sizing: border-box;
-  color: #9FF2F5;
-  letter-spacing: 0.16px;
-}
-
-.principal .botones {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-}
-.menu .botones .btn2 {
-  display: flex;
-  flex-direction: column;
-  align-items: center; 
-  margin-bottom: 10%;  
-}
-
-.botones .btn-acento {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 30px;
-  line-height: 35px;
-  color: #FAD8D6;
-  text-transform: uppercase;
-  background-color: #EE596D;
-  height: 67px;
-  width: 409px;
-  border-radius: 40px;
-  border:none;
-  margin: 5% 0% 10% 0%;
-  cursor:pointer;
-  transition: all 1s ease;
-
-}
-
-.botones .btn-primary {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 30px;
-  line-height: 35px;
-  color: #FAD8D6;
-  text-transform: uppercase;
-  background-color: #00A6D0;
-  height: 67px;
-  width: 409px;
-  border-radius: 40px;
-  border:none;
-  margin: 5% 0% 10% 0%;
-  cursor:pointer;
-  transition: all 1s ease;
-}
-
-.btn-primary:hover{
-  background-color: #00002E;
-}
-.btn-acento:hover{
-  background-color: #847996;
-}
-
-.menu .botones .btn2 .btn-primary{
-  font-size: 20px;
-  text-transform: capitalize;
-  height: 48px;
-  width: 272px;
-  border-radius: 30px;
-  padding-left: 27px;
-}
-.menu .botones a{
-  text-decoration: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #FAD8D6;
+<style scoped>
+.login-layout {
+  height: 100vh;
+  background: rgb(0, 153, 255);
 }
 </style>
 
--- src/modules/login/pages/LoginAuth.vue
+++ src/modules/login/pages/LoginAuth.vue
@@ -1,24 +1,21 @@
 <template>
- <section class="principal">
-    <div class="menu">
-      <h1>Sistema de Gestión<br>Odontológica</h1>
-      <h3>BIENVENIDO</h3>
-
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Direccion de mail</label>
-          <input type="email" class="btn-primary" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Direccion de email">
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-          <input type="password" class="btn-primary" id="exampleInputPassword1" placeholder="Contraseña">
-        </div>
-        
-        <RouterLink to="/home-page"><button type="submit" class="btn-acento">Ingresar</button></RouterLink>
-      </form>
-    </div>
-
-  </section>
+  <h2>login auth</h2>
+  <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+   <RouterLink to="/home-page"><button type="submit" class="btn btn-primary">Submit</button></RouterLink>
+</form>
 </template>
 
 <script>
@@ -27,103 +24,6 @@ export default {
 }
 </script>
 
-<style scoped>
-.principal {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-image: url("../../../assets/fondo.png");
-  background-size: cover;
-  background-position: center;
+<style>
 
-}
-
-.principal .menu {
-  width: 43%;
-  padding: 2% 0%;
-  box-sizing: border-box;
-  text-align: center;
-  position: relative;
-  margin-left: 199px;
-  background: rgba(0, 0, 0, 0.51);
-  line-height: 0;
-}
-
-h1 {
-
-  font-style: italic;
-  font-weight: 900;
-  font-size: 60px;
-  line-height: 70px;
-  letter-spacing: 0.16px;
-  color: #ffff;
-}
-
-.menu h3 {
-  font-style: normal;
-  font-weight: 500;
-  line-height: 53px;
-  font-size: 45px;
-  margin-top: 10%;
-  margin-bottom: 10%;
-  padding: 10% 0% 10% 0%;
-  box-sizing: border-box;
-  color: #9FF2F5;
-  letter-spacing: 0.16px;
-}
-.btn-acento {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 30px;
-  line-height: 35px;
-  color: #FAD8D6;
-  text-transform: uppercase;
-  background-color: #EE596D;
-  height: 67px;
-  width: 409px;
-  border-radius: 40px;
-  border: none;
-  margin: 5% 0% 10% 0%;
-  cursor: pointer;
-  transition: all 1s ease;
-
-}
-
-.btn-primary {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 30px;
-  line-height: 35px;
-  color: #FAD8D6;
-  text-transform: uppercase;
-  background-color: #00A6D0;
-  height: 67px;
-  width: 409px;
-  border-radius: 40px;
-  border: none;
-  margin: 5% 0% 10% 0%;
-  cursor: pointer;
-  transition: all 1s ease;
-}
-
-.btn-primary:hover {
-  background-color: #00002E;
-}
-
-.btn-acento:hover {
-  background-color: #847996;
-}
-.mb-3{
-  display: flex;
-    align-items: center;
-    flex-direction: column;
-}
-.menu .mb-3 .btn-primary {
-  font-size: 20px;
-  text-transform: capitalize;
-  height: 48px;
-  width: 272px;
-  border-radius: 30px;
-  padding-left: 27px;
-}
 </style>
